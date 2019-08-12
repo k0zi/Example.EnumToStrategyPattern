c5513eac5db000604ac461d29a15d683b60f71a8
k0zi <thekozi@gmail.com>
2019-08-12 17:19:07 +0200

    Tests

--- Example.EnumToStrategyPattern.Tests/UnitTest1.cs
+++ Example.EnumToStrategyPattern.Tests/UnitTest1.cs
@@ -3,11 +3,34 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Example.EnumToStrategyPattern.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class CalculatorTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Calculator CreateCalculator(decimal number, MyTypes type)
         {
+            return new Calculator() { Number = number, Type = type };
+        }
+        [TestMethod]
+        public void Calculate_First_100()
+        {
+            var calculator = CreateCalculator(100, MyTypes.First);
+            var result = calculator.Calculate();
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod]
+        public void Calculate_Second_200()
+        {
+            var calculator = CreateCalculator(100, MyTypes.Second);
+            var result = calculator.Calculate();
+            Assert.AreEqual(200, result);
+        }
+
+        [TestMethod]
+        public void Calculate_Third_300()
+        {
+            var calculator = CreateCalculator(100, MyTypes.Third);
+            var result = calculator.Calculate();
+            Assert.AreEqual(300, result);
         }
     }
 }
